983c396c321fd6b3893e597c6aa091466c9efc02
ThangNT <ntthang1121@gmail.com>
2021-09-27 11:20:48 +0700

    chapter_8

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
    root "static_pages#home"
    get "static_pages/help"
   end
+
   get "/users", to: "users#show"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
 
   resources :users, only: %i(new create show)
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+
 end
